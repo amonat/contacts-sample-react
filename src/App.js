f95bd0e813a77ec84ad88adb3618044f1ea6a028
Andy Monat <amonat@gmail.com>
2021-03-26 10:45:57 -0400

    Add Contacts component
    Currently this just shows the text "Contacts"

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Contacts from './Contacts';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Contact Tracker</h1>
+        <Contacts />
       </header>
     </div>
   );
