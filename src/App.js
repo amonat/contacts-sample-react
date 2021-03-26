3c76d94c14e4ddf6a0ddcccec30fe5f4ab2d8847
Andy Monat <amonat@gmail.com>
2021-03-26 14:06:17 -0400

    Add 3 hard-coded contacts to be used later.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,33 @@ import './App.css';
 import Contacts from './Contacts';
 
 function App() {
+  const contacts = [
+    {
+      id: 1,
+      firstName: 'Evelyn',
+      lastName: 'Hancock',
+      email: 'evelyn.hancock@example.com',
+      phone: '111-555-1212',
+      status: 'active',
+    },
+    {
+      id: 2,
+      firstName: 'Crystal',
+      lastName: 'Haas',
+      email: 'crystal.haas@example.com',
+      phone: '222-555-1213',
+      status: 'active',
+    },
+    {
+      id: 3,
+      firstName: 'Esteban',
+      lastName: 'Pope',
+      email: 'esteban.pope@example.com',
+      phone: '333-555-1212',
+      status: 'inactive',
+    },
+  ]
+
   return (
     <div className="App">
       <header className="App-header">
