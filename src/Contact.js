22fc4851302b514c1386161d06eb869b904a92d7
Andy Monat <amonat@gmail.com>
2021-03-27 16:19:47 -0400

    Concatenate first and last names, rather than using a non-breaking space.
    In the name of a contact, display the first name and then the last name with
    a normal space between them. This is both simpler to read and also makes testing
    easier.

--- src/Contact.js
+++ src/Contact.js
@@ -4,7 +4,7 @@ function Contact({ contact }) {
   return (
     <React.Fragment key={contact.id}>
       <h3 className='contact'>
-        {contact.firstName}&nbsp;{contact.lastName}
+        {`${contact.firstName} ${contact.lastName}`}
       </h3>
       <p>{contact.email}</p>
       <p>{contact.phone}</p>
