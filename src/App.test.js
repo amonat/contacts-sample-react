9c98a2f04dc291f834fe89283d848a86e74db7db
Andy Monat <amonat@gmail.com>
2021-03-28 13:58:27 -0400

    Add a test for editing a contact through the full UI.

--- src/App.test.js
+++ src/App.test.js
@@ -9,10 +9,26 @@ const setFirstName = (value) => { setTextField('First name', value) };
 const setLastName = (value) => { setTextField('Last name', value) };
 const setEmail = (value) => { setTextField('Email', value) };
 const setPhone = (value) => { setTextField('Phone', value) };
-const setActive = (value) => {
+const toggleActive = () => {
   const activeCheckbox = screen.getByLabelText('Active');
   fireEvent.click(activeCheckbox);
 };
+
+const addContact = ({ firstName, lastName, email, phone, active }) => {
+  const addContactButton = screen.getByRole('button', { name: 'Add contact' } );
+  fireEvent.click(addContactButton);
+
+  setFirstName(firstName);
+  setLastName(lastName);
+  setEmail(email);
+  setPhone(phone);
+  if (!active) {
+    toggleActive();
+  }
+  const saveButton = screen.getByRole('button', { name: 'Save contact' });
+  fireEvent.click(saveButton);
+};
+
 
 test('renders title', () => {
   render(<App />);
@@ -23,16 +39,15 @@ test('renders title', () => {
 describe('adding a contact', () => {
   it('allows filling and saving then displays a newly created contact', () => {
     render(<App />);
-    const addContactButton = screen.getByRole('button', { name: 'Add contact' } );
-    fireEvent.click(addContactButton);
+    const contactInfo = {
+      firstName: 'New',
+      lastName: 'Person',
+      email: 'newemail',
+      phone: 'newphone',
+      active: false,
+    };
 
-    setFirstName('New');
-    setLastName('Person');
-    setEmail('newemail');
-    setPhone('newphone');
-    setActive(false);
-    const saveButton = screen.getByRole('button', { name: 'Save contact' });
-    fireEvent.click(saveButton);
+    addContact(contactInfo);
 
     expect(screen.queryByRole('alert')).toBeNull();
     expect(screen.queryByRole('button', { name: 'Save contact' })).toBeNull();
@@ -46,4 +61,39 @@ describe('adding a contact', () => {
     const newContactActive = screen.getByText(/inactive/i);
     expect(newContactActive).toBeInTheDocument();
   });
+});
+
+test('edits a contact', () => {
+  render(<App />);
+
+  const initialContactInfo = {
+    firstName: 'InitialFirst',
+    lastName: 'IntialLast',
+    email: 'initial@example.org',
+    phone: '212-INI-TIAL',
+    active: true,
+  };
+
+  addContact(initialContactInfo);
+
+  const editButton = screen.getByRole('button', { name: 'Edit' });
+  fireEvent.click(editButton);
+
+  setFirstName('EditedFirst');
+  setLastName('EditedLast');
+  setEmail('edited@example.org');
+  setPhone('212-44F-INAL');
+  toggleActive();
+
+  const saveButton = screen.getByRole('button', { name: 'Save contact' });
+  fireEvent.click(saveButton);
+
+  const newContactName = screen.getByText(/EditedFirst EditedLast/);
+  expect(newContactName).toBeInTheDocument();
+  const newContactEmail = screen.getByText(/edited@example.org/);
+  expect(newContactEmail).toBeInTheDocument();
+  const newContactPhone = screen.getByText(/212-44F-INAL/);
+  expect(newContactPhone).toBeInTheDocument();
+  const newContactActive = screen.getByText(/inactive/i);
+  expect(newContactActive).toBeInTheDocument();
 });
